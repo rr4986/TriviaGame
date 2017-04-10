531e7dc7d9652dad10d5651560f655fb1ade39ea
Ruben Reyes <ruben.r.reyes@gmail.com>
2017-04-10 00:17:52 -0500

    working trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 
-var count=15;
+var count=30;
 var numberRight=0;
 var numberWrong=0;
+
 
 window.onload = function() {
 
@@ -9,26 +10,56 @@ window.onload = function() {
 	intervalId = setInterval(timeUp, 1000);
 }
 
-$( "input" ).on( "click", function() {
-  if($( "input:checked" ).val()==="right" ){
-  	numberRight++;
-  };
-  if($( "input:checked" ).val()==="wrong" ){
-  	numberWrong++;
-  }
-});
 
 function timeUp() {
+
 
 	count--;
 	$("#timer").html("<h1>Time: "+count+" seconds</h2>");
   	if(count<1){
-  		$(".container").html("<h1>Time's Up 2 <br><br>"+
+
+  		if($('input[name=firstquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=firstquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+  		if($('input[name=secondquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=secondquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+  		if($('input[name=thirdquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=thirdquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+  		if($('input[name=fourthquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=fourthquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+  		if($('input[name=fifthquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=fifthquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+  		if($('input[name=sixthquestion]:checked').val()==="right"){
+  			numberRight++;
+  		}
+  		if($('input[name=sixthquestion]:checked').val()==="wrong"){
+  			numberWrong++;
+  		}
+
+  		$(".container").html("<h1>Time's Up <br><br>"+
  		"<h1>You got "+numberRight+" Correct<br>"+"You got "+numberWrong+" Wrong</h1>");
   		clearInterval(intervalId);
 
   	}
 
 }
-
 
